da8aa98964a3ad5c4f9b47981837000165157420
Mr.Da <836160973da@gmail.com>
2017-10-28 20:58:45 +0800

    update

--- Photography/Struts2+Hibernate/src/com/da/Photography/daoImpl/AlbumsHibDao.java
+++ Photography/Struts2+Hibernate/src/com/da/Photography/daoImpl/AlbumsHibDao.java
@@ -1,11 +1,23 @@
 package com.da.Photography.daoImpl;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+
+import org.hibernate.Hibernate;
+import org.hibernate.SQLQuery;
+import org.hibernate.transform.Transformers;
 
 import com.da.Photography.dao.AlbumsDaoInterface;
 import com.da.Photography.dto.Albums;
 import com.da.Photography.dto.Picture;
+import com.da.Photography.dto.User;
+import com.da.Photography.entity.PaAlbums;
+import com.da.Photography.entity.PaUser;
+
+import sun.security.krb5.internal.PAData;
 
 public class AlbumsHibDao extends BaseHibDao implements AlbumsDaoInterface {
 	/**
@@ -17,8 +29,27 @@ public class AlbumsHibDao extends BaseHibDao implements AlbumsDaoInterface {
 	 */
 	@Override
 	public List<Albums> queryAlbums(int u_id, String stat) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		List<Albums> albums = new ArrayList<>();
+		String sql = "Select a.A_ID, a.A_NAME, a.A_TIME, a.A_PROFILE, u.U_NAME "
+				+ "From PA_ALBUMS a, PA_USER u "
+				+ "Where u.U_ID = a.U_ID";
+		if(stat.equals("1")) {
+			sql += " And a.U_ID = " + u_id;
+		}
+		sql += " order by a_id ";
+		SQLQuery sq = session.createSQLQuery(sql);
+		Iterator it = sq.list().iterator();
+		while(it.hasNext()){
+			Object[] obj = (Object[]) it.next();
+			Albums a = new Albums();
+			a.setA_id(Integer.valueOf(obj[0].toString()));
+			a.setA_name(obj[1].toString());
+			a.setA_time((Date) obj[2]);
+			a.setA_profile(obj[3].toString());
+			a.getUser().setU_name(obj[4].toString());
+			albums.add(a);
+		}
+		return albums;
 	}
 	/**
 	 * 添加专辑
@@ -29,8 +60,12 @@ public class AlbumsHibDao extends BaseHibDao implements AlbumsDaoInterface {
 	 */
 	@Override
 	public int addAlbums(int u_id, Albums albums) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		int result = 0;
+		String sql = "insert into pa_albums "
+				+ "values((select nvl(max(a_id),0)+1 from pa_albums),?,?,sysdate,?,?)";
+//		Object[] params = {albums.getA_name(),u_id,albums.getA_profile(),albums.getA_pic()};
+//		result = executeUpdate(sql, params);
+		return result;
 	}
 	/**
 	 * 查询专辑的封面图片通过专辑编号
