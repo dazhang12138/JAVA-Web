72381e263d201d1b4b2a92a05cfda19e27c70b72
Mr.Da <836160973da@gmail.com>
2017-12-13 09:08:18 +0800

    create UserDao

--- DaBaiNetworkStorage/dabai/src/main/java/com/da/dabai/dao/UserDao.java
+++ DaBaiNetworkStorage/dabai/src/main/java/com/da/dabai/dao/UserDao.java
@@ -13,8 +13,9 @@ public class UserDao {
 	 * 通过查询信息查询用户是否存在
 	 * @param document 查询条件
 	 * @return 返回用户信息，如果用户不存在则返回null
+	 * @throws Exception
 	 */
-	public Document queryUser(Document document) {
+	public Document queryUser(Document document) throws Exception {
 		Document d = null;
 		MongoDatabase mdb = MongoDBUtil.instance.getMdb();
 		MongoCollection<Document> docs = mdb.getCollection("dbUser");
@@ -23,6 +24,28 @@ public class UserDao {
 			d = document2;
 		}
 		return d;
+	}
+
+	/**
+	 * 保存用户信息
+	 * @param document 用户信息
+	 * @throws Exception
+	 */
+	public void saveUser(Document document) throws Exception {
+		MongoDatabase mdb = MongoDBUtil.instance.getMdb();
+		MongoCollection<Document> docs = mdb.getCollection("dbUser");
+		docs.insertOne(document);
+	}
+
+	/**
+	 * 保存文件夹信息
+	 * @param fileDoc 文件夹信息
+	 * @throws Exception 
+	 */
+	public void saveFiles(Document fileDoc) throws Exception {
+		MongoDatabase mdb = MongoDBUtil.instance.getMdb();
+		MongoCollection<Document> docs = mdb.getCollection("dbFiles");
+		docs.insertOne(fileDoc);
 	}
 
 }
