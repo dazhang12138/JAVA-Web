333921082fcf9f003c7a12518d445b798d960681
Mr.Da <836160973da@gmail.com>
2018-01-03 09:32:19 +0800

    create URL Path

---
+++ DaBaiNetworkStorage/yylib-sample/apps/SampleApp/modules/main/Url.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ICOP on 2017-10-24.
+ */
+
+var PATH = "http://localhost:8080/";
+var CODE = "dabai";
+
+var MainUrl = {
+    LOGIN: PATH + CODE +  "/User/Login",
+    VERIFY: PATH + CODE + "/User/UserVerify",
+    REGISTER: PATH + CODE + "/User/Register",
+}
+module.exports = MainUrl;
