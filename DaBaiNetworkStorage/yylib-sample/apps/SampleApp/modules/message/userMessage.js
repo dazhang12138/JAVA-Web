0e61c62601c56320162cec1c7e47e63068aac6f1
Mr.Da <836160973da@gmail.com>
2018-01-22 14:39:59 +0800

    添加个人信息页面的各类文件存储信息

--- DaBaiNetworkStorage/yylib-sample/apps/SampleApp/modules/message/userMessage.js
+++ DaBaiNetworkStorage/yylib-sample/apps/SampleApp/modules/message/userMessage.js
@@ -2,11 +2,138 @@ var React = require('react');
 var _ = require('lodash');
 var {YYClass} = require('yylib-ui');
 var {YYCreatePage} =  require('yylib-business');
+var YYChart = require('yylib-ui/chart/YYChart');
+var ReactDOM = require('react-dom');
 var ajax = require('yylib-utils/ajax');
 var ReduxUtils = require('yylib-utils/ReduxUtils');
+var URL = require('./Url');
 var THIS;
 var UserDate;
-//页面初始化
+//图表变量
+var Chart;
+
+/*各类文件类型使用情况图表*/
+const options = {
+    tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b}: {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        x: 'left',
+        data:['未使用','文档','视频','音频','图片','种子','其他']
+    },
+    series: [
+        {
+            name:'各类文件使用控件统计',
+            type:'pie',
+            radius: ['50%', '70%'],
+            avoidLabelOverlap: false,
+            label: {
+                normal: {
+                    show: false,
+                    position: 'center'
+                },
+                emphasis: {
+                    show: true,
+                    textStyle: {
+                        fontSize: '30',
+                        fontWeight: 'bold'
+                    }
+                }
+            },
+            labelLine: {
+                normal: {
+                    show: false
+                }
+            },
+            data:[]
+        }
+    ]
+};
+
+var series = {
+    name:'各类文件使用控件统计',
+    type:'pie',
+    radius: ['50%', '70%'],
+    avoidLabelOverlap: false,
+    label: {
+        normal: {
+            show: false,
+            position: 'center'
+        },
+        emphasis: {
+            show: true,
+            textStyle: {
+                fontSize: '30',
+                fontWeight: 'bold'
+            }
+        }
+    },
+    labelLine: {
+        normal: {
+            show: false
+        }
+    },
+    data:[
+        {value:335, name:'未使用'},
+        {value:310, name:'视频'},
+        {value:234, name:'音频'},
+        {value:135, name:'图片'},
+        {value:1548, name:'种子'},
+        {value:10, name:'其他'}
+    ]
+};
+
+/*图表实例*/
+var Mychart = YYClass.create({
+    ready:function (chart) {
+        chart.on('click', function (params) {
+            console.log('监测待办排行',params);
+        });
+        Chart = chart;
+        queryFileTypeNumber();
+    },
+    render:function () {
+        return (
+            <div>
+                <YYChart {...options} onReady={this.ready}>
+                    <YYChart.Bar {...series} />
+                </YYChart>
+            </div>
+        )
+    }
+});
+
+/*图表*/
+var ChartDemo1 = YYClass.create({
+    componentDidMount: function () {
+        ReactDOM.render(<Mychart />, document.getElementById('chartdemo1'));
+    },
+    render: function () {
+        return <div id="chartdemo1"></div>
+    }
+})
+
+
+/*查询用户存储空间使用情况的详情*/
+function queryFileTypeNumber() {
+    var data = {
+        userId : UserDate._id,
+    };
+    ajax.postJSON(URL.SPACESTORAGE,data,function (result) {
+        var redata = options.series[0].data = [];
+        for (var i = 0; i < result.length; i++){
+            var d = {
+                name:result[i].name,
+                value:result[i].size,
+            };
+            redata.push(d);
+        }
+        Chart.setOption(options);
+    });
+}
+
 
 /*根据出生日期算出年龄*/
 function jsGetAge(strBirthday){
@@ -47,6 +174,11 @@ function saveAge(date) {
     THIS.refresh();
 }
 var EventHanlder = {
+    "goBack":{
+        onClick:function () {
+            THIS.goBack();
+        }
+    },
     "saveMessage":{
         onClick:function () {
             var basicInformation = THIS.findUI('basicInformation');
@@ -71,6 +203,11 @@ var EventHanlder = {
         onViewWillMount:function(options){
             console.log('page onViewWillMount',options);
             THIS = this;
+            var routeData = THIS.getRouteQuery();
+            if(routeData != null){
+                var message = THIS.findUI('message');
+                message.defaultActiveKey = '11516333536623_91632';
+            }
             UserDate = THIS.getRouteParams();
             var name = THIS.findUI('name');
             name.value = UserDate.name;
@@ -100,9 +237,14 @@ var EventHanlder = {
         }
     }
 }
+
+var MyParser = {};
+MyParser.saveChart = ChartDemo1;
+
 var SimplePage = YYClass.create({
     render:function(){
-        return <YYCreatePage {...this.props} appCode="A000776" pageCode="P005076" uiEvent={EventHanlder}/>
+        return <YYCreatePage {...this.props} appCode="A000776" pageCode="P005076" uiEvent={EventHanlder}
+                             uiParser={MyParser}/>
     }
 });
 module.exports = SimplePage;
